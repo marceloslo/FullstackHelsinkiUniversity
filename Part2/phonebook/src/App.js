80eb81b4c1bcab385e4993c90f153f2d43d618f4
marceloslo <marcelosartlocatelli@gmail.com>
2023-02-03 12:44:46 -0300

    2.16-2.17

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -4,17 +4,45 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import personService from './services/persons'
 
+
+const Notification = ({message}) =>{
+  if(message === null){
+    return null
+  }
+  return (
+    <div className={message.className}>
+      {message.message}
+    </div>
+  )
+}
+
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [fil, setNewFilter] = useState('')
+  const [message, setMessage] = useState({message:null,className:null})
 
   useEffect(()=>{
     personService
       .getAll()
       .then(persons => setPersons(persons))
   },[])
+
+  const successMessage = (personName) =>{
+    setMessage({message:`Added ${personName}`,className:'success'})
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
+
+  const errorMessage = (personName) =>{
+    setMessage({message:`Information of ${personName} has already been removed from server`,className:'error'})
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
 
   const addPerson = (event) =>{
     event.preventDefault()
@@ -27,6 +55,7 @@ const App = () => {
         setPersons(persons.map(person => person.id === returnedPerson.id ? returnedPerson : person))
         setNewName('')
         setNewNumber('')
+        successMessage(returnedPerson.name)
       })
       return
     }
@@ -37,6 +66,7 @@ const App = () => {
       setPersons(persons.concat(returnedPerson))
       setNewName('')
       setNewNumber('')
+      successMessage(returnedPerson.name)
     })
   }
 
@@ -57,12 +87,19 @@ const App = () => {
     const confirm = window.confirm(`delete ${personToDelete.name}?`)
     if(confirm)
     {
-      personService.remove(id).then(response => setPersons(persons.filter(person => person.id !== id)))
+      personService
+        .remove(id)
+        .then(response => setPersons(persons.filter(person => person.id !== id)))
+        .catch(error => {
+          errorMessage(personToDelete.name)
+          setPersons(persons.filter(person => person.id !== id))
+        })
     }
   }
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message}/>
       <Filter value={fil} onChange={updateFilter}/>
       <h2>Add a new</h2>
       <PersonForm onSubmit={addPerson} 
