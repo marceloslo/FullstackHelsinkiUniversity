774c25c17b7ddcdb46084301e7d03510f76a8959
marceloslo <marcelosartlocatelli@gmail.com>
2023-02-03 17:46:24 -0300

    2.18-2.20

--- Part2/countries/src/services/weather.js
+++ Part2/countries/src/services/weather.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const url ='https://api.openweathermap.org/data/2.5/weather?'
-const api_key = '16e95345e3c4e71904f616ed81679048'
+const api_key = process.env.REACT_APP_API_KEY
 const getWeather = (lat,lon) => {
     return axios.get(`${url}lat=${lat}&lon=${lon}&appid=${api_key}`).then(response => response.data)
 }
