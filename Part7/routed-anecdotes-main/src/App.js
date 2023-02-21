8421c8a88388bf874828ea39d3ca38f5638142c2
marceloslo <marcelosartlocatelli@gmail.com>
2023-02-21 09:50:44 -0300

    7.4-7.6

--- Part7/routed-anecdotes-main/src/App.js
+++ Part7/routed-anecdotes-main/src/App.js
@@ -4,6 +4,10 @@ import {
   Routes, Route, Link, useMatch, useNavigate
 } from 'react-router-dom'
 
+import { useField } from './hooks'
+
+
+
 const Notification = ({notification}) => {
   if(notification === null){
     return null
@@ -70,20 +74,20 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const {reset: resetContent,...content} = useField('text')
+  const {reset: resetAuthor,...author} = useField('text')
+  const {reset: resetInfo,...info} = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content:content.value,
+      author:author.value,
+      info:info.value,
       votes: 0
     })
-    props.setNotification(`a new anecdote ${content} created!`)
+    props.setNotification(`a new anecdote ${content.value} created!`)
     setTimeout(()=>{
       props.setNotification(null)
     },5000)
@@ -96,18 +100,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
+      <button onClick={() => {resetInfo();resetAuthor();resetContent()}}>reset</button>
     </div>
   )
 
